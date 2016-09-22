9d9aa3c3a4311a46523832575a0302e3f7b3bb86
Don Patterson <d_j_p_3@djp3.net>
2016-09-22 10:32:24 -0700

    Setting up continuous integration with Travis

---
+++ src/edu/westmont/cs130/server/myServer.java
@@ -0,0 +1,12 @@
+package edu.westmont.cs130.server;
+
+public class myServer {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Starting the server");
+
+		System.out.println("Quitting the server");
+	}
+
+}
