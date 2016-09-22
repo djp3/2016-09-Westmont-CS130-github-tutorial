ee8cd5de35b4bc421ef67e853e0b471780f6f0ac
CHOLHYUNPARK\cholhyun <chpark>
2016-09-22 15:28:45 -0700

    new commit

--- src/edu/westmont/cs130/server/MyServer.java
+++ src/edu/westmont/cs130/server/MyServer.java
@@ -11,6 +11,7 @@ public class MyServer {
 		System.out.println("Starting the server");
 		System.out.println("Prof. Patterson made a change");
 		System.out.println("\tCaleb was here");
+		System.out.println("\t Chol is here");
 		System.out.println("Quitting the server");
 	}
 
