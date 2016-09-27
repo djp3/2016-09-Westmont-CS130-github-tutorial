334914568ea069daffe36a3d3d4e95256c086856
Don Patterson <d_j_p_3@djp3.net>
2016-09-27 14:25:47 -0700

    next

--- src/edu/westmont/cs130/client/MyClient.java
+++ src/edu/westmont/cs130/client/MyClient.java
@@ -4,6 +4,22 @@ package edu.westmont.cs130.client;
 public class MyClient {
 
 	public static void main(String[] args) {
+
+		// Make sure that the program is started correctly
+		if(args.length < 3){
+			System.out.println("Usage: MyClient <name> <ipaddress> <port>");
+			return;
+		}
+		
+		// Collect the parameters from the command line and error check them
+		String name = args[0];
+		String ipAddress = args[1];
+		String _port = args[2];
+		Integer port = Integer.valueOf(_port);
+		if((port <= 1024) || (port > 9999)){
+			System.out.println("Your port number seems to be out of range (1024 < port < 10000): \""+_port+"\"");
+			return;
+		}
 		
 	}
 
