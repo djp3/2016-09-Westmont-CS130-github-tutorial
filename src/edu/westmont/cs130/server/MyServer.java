d4fa15e0a9ba778d247d2f7e3f4ac98133c610cf
Don Patterson <d_j_p_3@djp3.net>
2016-09-27 13:29:39 -0700

    Step 1 in class

--- src/edu/westmont/cs130/server/MyServer.java
+++ src/edu/westmont/cs130/server/MyServer.java
@@ -1,13 +1,21 @@
 package edu.westmont.cs130.server;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-
 public class MyServer {
 
 	public static void main(String[] args) {
 		
+		// Make sure that the program is started correctly
+		if ((args.length < 1) || (args.length > 1)){
+			System.out.println("Usage: MyServer <port>");
+			return;
+		}
+		
+		// Collect the parameters from the command line and error check them
+		String _port = args[0];
+		Integer port = Integer.valueOf(_port);
+		if ((port <= 1024) || (port > 9999)) {
+			System.out.println("Your port number seems to be out of range (1024 < port < 10000): \"" + _port + "\"");
+			return;
+		}
 	}
 }
